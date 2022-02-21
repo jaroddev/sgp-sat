2367e1a79653764a23e05e0bace015c107f3400d
jarod <martinjarod17@gmail.com>
2022-02-21 09:33:31 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,30 @@
+# Project SGP: SAT part
+
+## Installation steps
+
+ - setup a venv and activate the source 
+ - install wheels and setuptools
+ - update pip
+ - install the requirements via the (pysat, etc...)
+
+## How to use it
+
+### use the CLI
+
+An example: 
+` python3 run.py -g 4 -s 2 -w 2`
+
+### Test runner (simple script)
+
+Follow the steps:
+ - Create the bench folder.
+ `mkdir bench`
+ 
+ - Run the script in the project's root directory.
+ `python3 client.py`
+
+ - You can run this command to find which configuration are UNSAT.
+ `grep -rni "UNSAT" bench/*.out`
+
+  - You can look at the csv files to watch the times for each g-p tuples.
+ `grep -rni "UNSAT" bench/*.out`
--- client.py
+++ client.py
@@ -10,12 +10,12 @@ MAX_WEEK = 9
 
 
 def shell_command(group_size, position, week):
-    return f"{CMD} -g {group_size} -s {position} -w {week} >> ./{BENCH_REPO}/{group_size}-{position}-{week}.out"
+    return f"{CMD} -g {group_size} -s {position} -w {week} > ./{BENCH_REPO}/{group_size}-{position}-{week}.out"
 
 
-def echo_csv(records):
-    filename = f"./{BENCH_REPO}/times.csv"
-    header = "group_size;position;week;elapsed\n"
+def echo_csv(group_size, position, records):
+    filename = f"./{BENCH_REPO}/{group_size}-{position}-times.csv"
+    header = "week;elapsed\n"
     
     with open(filename, 'w') as file:
         file.write(header)
@@ -24,17 +24,18 @@ def echo_csv(records):
         
 
 
-def time_record(group_size, position, week, time):
-    return f"{group_size};{position};{week};{time}\n"
+def record_time(group_size, position, week, time):
+    return f"{week};{time}\n"
 
 
 
 # Tests
 if __name__ == "__main__":
-    time_records = ""
 
     for group_size in range(1,MAX_GROUP_SIZE + 1):
         for position in range(1,MAX_POS + 1):
+            time_records = ""
+            
             for week in range(1,MAX_WEEK + 1):
 
                 cmd = shell_command(group_size, position, week)
@@ -43,9 +44,9 @@ if __name__ == "__main__":
                 subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
                 end = time()
 
-                record = time_record(group_size, position, week, end-start)
+                record = record_time(group_size, position, week, end-start)
 
                 time_records = f"{time_records}{record}"
     
-    echo_csv(time_records)
+            echo_csv(group_size, position, time_records)
 
